7fa3f6ce42ddeb666270268e3f0485f369402e7f
Bima0x01Async <49645185+bimaadam@users.noreply.github.com>
2026-02-16 18:57:26 +0700

    feat: add new Navbar component for site navigation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ export function Navbar() {
         <div class="nav-container">
             <a href="#" class="nav-logo">
                 <div class="nav-logo-icon">
-                    <img src="images/ignitron.png" alt="IgnitronDev">
+                    <img src="../images/ignitron.png" alt="IgnitronDev">
                 </div>
                 <div class="nav-logo-text">
                     <span class="nav-logo-name">Ignitron<span>Dev</span></span>
